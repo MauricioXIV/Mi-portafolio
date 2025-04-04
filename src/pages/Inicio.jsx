50a3d4246dbb10c20a02fe92659822c28276c719
Mauricio <mauballe11@gmail.com>
2025-04-03 21:49:34 -0600

    first commit

---
+++ src/pages/Inicio.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Inicio = () => {
+  return (
+    <div>
+      <h1>Inicio</h1>
+      <p>Bienvenido a mi portafolio</p>
+      <p>Esta es la sección de inicio.</p>
+    </div>
+  );
+}
+export default Inicio;
