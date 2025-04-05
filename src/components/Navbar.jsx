ce3a11121f82ae0c33e5c194935392eac0c5503d
Mauricio <mauballe11@gmail.com>
2025-04-05 16:53:36 -0600

    continuacion

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <div className="navbar">
             <div className="imagen">
-            <img className="w-9 h-9 justify-self-center rounded-full lg:ml-8 md:ml-10 sm:ml-16" src={elmau} />
+            <img className="w-9 h-9 justify-self-center rounded-full lg:ml-20 larguito:ml-24 md:ml-20 sm:ml-16" src={elmau} />
             </div>
             <div className="yo">Mauricio Rodríguez<div className="text-xxs">Web Developer</div></div>
             <div className="apartado pl-5">Inicio</div>
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FaFilePdf } from 'react-icons/fa';
-
+import ReactLogo from "../assets/react.svg";
+import Html from "../images/html.png";
 
 const Inicio = () => {
   return (
@@ -17,6 +18,50 @@ const Inicio = () => {
       </div>
       <div className="w-2/3 flex sm:justify-center my-4">
       <button className="boton bg-[#161612] border font-semibold flex items-center"><a className="flex items-center w-full" href="../public/curriculum.pdf" download="CurriculumMauricio"><FaFilePdf className="ml-2 w-1/6"/><div className="w-5/6 larguito:mr-4 lg:mr-0 xl:mr-6">Descargar CV</div></a></button>
+      </div>
+      <div className="imagenes">
+        <div className="aimag"><img src={ReactLogo} /></div>
+        <div className="aimag"><img
+        src="https://upload.wikimedia.org/wikipedia/commons/7/75/Django_logo.svg"
+        alt="Django Logo"
+        className="w-16 invert"
+      /></div>
+        <div className="aimag"><img
+        src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
+        alt="JavaScript Logo"
+        className="w-8"
+      /></div>
+        <div className="aimag"><img
+        src="https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg"
+        alt="Python Logo"
+        className="w-8"
+      /></div>
+        <div className="aimag"><img
+        src={Html}
+        alt="HTML5 Logo"
+        className="w-10"
+      /></div>
+        <div className="aimag"><img
+        src="https://upload.wikimedia.org/wikipedia/commons/6/62/CSS3_logo.svg"
+        alt="CSS3 Logo"
+        className="w-8"
+      /></div>
+        <div className="aimag"><img
+        src="https://upload.wikimedia.org/wikipedia/commons/e/e0/Git-logo.svg"
+        alt="Git Logo"
+        className="w-16"
+      /></div>
+      </div>
+    </div>
+    <div className="proyectos">
+      <div className="w-2/3"><h3>Proyectos</h3></div>
+      <div className="w-2/3"><h1 className="text-xl font-medium mb-4">Mis proyectos</h1></div>
+      <div className="w-2/3 flex rounded-lg text-center">
+        <div className="sm:w-2/3 md:w-1/2 lg:w-1/3 flex bg-gray-600 rounded-xl h-10 items-center">
+          <div className="w-1/3 text-gray-400 text-sm">Todos</div>
+          <div className="w-1/3 text-gray-400 text-sm">Full Stack</div>
+          <div className="w-1/3 text-gray-400 text-sm">Backend</div>
+        </div>
       </div>
     </div>
     </>
