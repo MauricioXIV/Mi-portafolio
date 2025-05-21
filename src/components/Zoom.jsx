8def5ded8896251847d3b881da32bec0d7ade8bf
Mauricio <mauballe11@gmail.com>
2025-05-21 00:17:32 -0600

    2nd attempt to fix Zoom

--- src/components/Zoom.jsx
+++ src/components/Zoom.jsx
@@ -3,7 +3,6 @@ import { motion } from "framer-motion";
 
 const ZoomPage = ({ children }) => (
     <motion.div
-      className="w-full h-[100dvh] overflow-hidden"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
